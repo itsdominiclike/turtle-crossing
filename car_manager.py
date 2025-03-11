23d634fe32ece8713521ca2d6d45f7b3698d5c9f
itsdominiclike <70446156+itsdominiclike@users.noreply.github.com>
2025-03-11 11:28:12 +0000

    basic car creation shape is created, shapesize created, color can be defined when object is created as well as xpos and ypos (for now)

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,22 @@
+from turtle import Turtle
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+    def __init__(self, color, x_pos, y_pos):
+        super().__init__()
+        self.car_color = color
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.create_car()
+
+    def create_car(self):
+        self.penup()
+        self.shapesize(1,2,None)
+        self.shape('square')
+        self.color(self.car_color)
+        self.goto(self.x_pos, self.y_pos)
+
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from scoreboard import Scoreboard
 
 player = Player()
 
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
