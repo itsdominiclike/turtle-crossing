688725184a9fb6673ae0678a11ec85bfd444c3c0
itsdominiclike <70446156+itsdominiclike@users.noreply.github.com>
2025-03-11 13:21:43 +0000

    made height and width global variables, created additional global variables for playable area split into top and bottom halves

--- car_manager.py
+++ car_manager.py
@@ -1,22 +1,30 @@
 from turtle import Turtle
+import random
+
+from main import HEIGHT
 
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
+START_X = 320
+
+PLAYABLE_TOP = HEIGHT/2
+PLAYABLE_BOTTOM = (HEIGHT/2)*-1
 
 
 class CarManager(Turtle):
-    def __init__(self, color, x_pos, y_pos):
+    def __init__(self):
         super().__init__()
-        self.car_color = color
-        self.x_pos = x_pos
-        self.y_pos = y_pos
+        # self.x_pos = x_pos
+        # self.y_pos = y_pos
         self.create_car()
+        self.randcolor = random.choice(COLORS)
+
 
     def create_car(self):
         self.penup()
         self.shapesize(1,2,None)
         self.shape('square')
-        self.color(self.car_color)
-        self.goto(self.x_pos, self.y_pos)
+        self.color(self.randcolor)
+
 
--- main.py
+++ main.py
@@ -4,11 +4,14 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
+HEIGHT = 600
+WIDTH = 600
+
 player = Player()
 
 
 screen = Screen()
-screen.setup(width=600, height=600)
+screen.setup(WIDTH, HEIGHT)
 screen.tracer(0)
 
 # basic key input listener - can make it similar to the pong game if needed
@@ -22,4 +25,5 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    car = CarManager()
 
