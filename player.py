9e9a9c1f0ae97b2fa1fa542b8e9a4c0456b4de7f
itsdominiclike <70446156+itsdominiclike@users.noreply.github.com>
2025-03-11 11:12:39 +0000

    basic movement logic for turtle basic input detection to move turtle

--- player.py
+++ player.py
@@ -1,7 +1,31 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape('turtle')
+        self.color('black')
+        self.penup()
+        self.goto(STARTING_POSITION)
+        self.setheading(90)
+        self.start_x = 0
+        self.start_y = -280
+
+    # current movement logic (added ability to also go down, left and right)
+    def move_up(self):
+        self.start_y += MOVE_DISTANCE
+        self.goto(self.start_x, self.start_y)
+    def move_down(self):
+        self.start_y -= MOVE_DISTANCE
+        self.goto(self.start_x, self.start_y)
+    def move_right(self):
+        self.start_x += MOVE_DISTANCE
+        self.goto(self.start_x, self.start_y)
+    def move_left(self):
+        self.start_x -= MOVE_DISTANCE
+        self.goto(self.start_x, self.start_y)
