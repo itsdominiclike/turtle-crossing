e8576f1f1a459ba0f83b81e0625b46b23d061f23
itsdominiclike <70446156+itsdominiclike@users.noreply.github.com>
2025-03-11 11:10:20 +0000

    Added main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+player = Player()
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+# basic key input listener - can make it similar to the pong game if needed
+screen.listen()
+screen.onkeypress(player.move_up, "Up")
+screen.onkeypress(player.move_down, "Down")
+screen.onkeypress(player.move_left, "Left")
+screen.onkeypress(player.move_right, "Right")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
